ab7acb1fed8042d311a98ffa8186598dbc4ca804
Timm Haucke <timm@haucke.xyz>
2022-08-11 19:18:06 +0200

    add weight download script

---
+++ weights/download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -P "$(dirname $0)" https://f002.backblazeb2.com/file/share-twgb3cchwo4679bm/distance_estimation/weights/dpt_hybrid-midas-6c3ec701.onnx
+wget -P "$(dirname $0)" https://f002.backblazeb2.com/file/share-twgb3cchwo4679bm/distance_estimation/weights/md_v5a.0.0.onnx
