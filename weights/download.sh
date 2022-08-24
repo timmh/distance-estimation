06fe8c95b80e75b4bf882e3c8abd09b7337a19fa
Timm Haucke <timm@haucke.xyz>
2022-08-24 21:30:26 +0200

    use updated MegaDetectorLite weights with smaller minimum detection confidence threshold

--- weights/download.sh
+++ weights/download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 wget --content-disposition -P "$(dirname $0)" https://github.com/timmh/DPT/releases/download/onnx_v0.1/dpt_hybrid-midas-6c3ec701.onnx
-wget --content-disposition -P "$(dirname $0)" https://github.com/timmh/MegaDetectorLite/releases/download/v0.1/md_v5a.0.0.onnx
+wget --content-disposition -P "$(dirname $0)" https://github.com/timmh/MegaDetectorLite/releases/download/v0.2/md_v5a.0.0.onnx
