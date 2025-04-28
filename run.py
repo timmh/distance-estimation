fix incorrect computation of world coordinates

run.py
@@ -298,19 +298,19 @@ def run(config: Config):
                     # compute horizontal angle a
                     f = (0.5 * depth.shape[1]) / math.tan(0.5 * math.pi * config.camera_horizontal_fov / 180)
                     c = np.array([0, 0, f])
-                    p = np.array([0, (box[0] + box[2]) / 2 - depth.shape[1] / 2, f])
+                    p = np.array([(box[0] + box[2]) / 2 - depth.shape[1] / 2, 0, f])
                     a = math.copysign(1, (box[0] + box[2]) / 2 - depth.shape[1] / 2) * math.acos((c @ p) / (np.linalg.norm(c) * np.linalg.norm(p)))
 
                     # compute vertical angle b
                     f = (0.5 * depth.shape[0]) / math.tan(0.5 * math.pi * config.camera_vertical_fov / 180)
                     c = np.array([0, 0, f])
-                    p = np.array([(box[1] + box[3]) / 2 - depth.shape[0] / 2, 0, f])
+                    p = np.array([0, (box[1] + box[3]) / 2 - depth.shape[0] / 2, f])
                     b = math.copysign(1, (box[1] + box[3]) / 2 - depth.shape[0] / 2) * math.acos((c @ p) / (np.linalg.norm(c) * np.linalg.norm(p)))
 
                     # compute world position
-                    x = sampled_depths[-1] * math.tan(a)
-                    y = sampled_depths[-1] * math.tan(b)
-                    z = sampled_depths[-1] * math.cos(a) * math.cos(b)
+                    z = sampled_depths[-1] / math.sqrt(math.tan(a) ** 2 + math.tan(b) ** 2 + 1)
+                    x = z * math.tan(a)
+                    y = z * math.tan(b)
                     world_positions += [[x, y, z]]
 
                     if config.multiple_animal_reduction == MultipleAnimalReduction.ONLY_CENTERMOST: