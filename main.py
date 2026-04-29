f358a0284d7e3265f75ee12c4606d68069549d4c
Timm Haucke <timm@haucke.xyz>
2026-04-28 21:01:03 -0400

    fix checkbox update handler

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def build_config_inputs(config, container, on_config_change):
         elif value_type is int:
             input = toga.NumberInput(value=value, step=1, on_change=lambda input, var=var: on_config_change(var, int(input.value)))
         elif value_type is bool:
-            input = toga.Switch("", value=value, on_change=lambda input, var=var: on_config_change(var, input.is_on))
+            input = toga.Switch("", value=value, on_change=lambda input, var=var: on_config_change(var, input.value))
         elif issubclass(value_type, Enum):
             input = toga.Selection(items=[e for e in dir(type(value)) if not e.startswith("_")], on_change=lambda input, var=var, default_type=value_type: on_config_change(var, getattr(default_type, input.value)))
             input.value = value.name
